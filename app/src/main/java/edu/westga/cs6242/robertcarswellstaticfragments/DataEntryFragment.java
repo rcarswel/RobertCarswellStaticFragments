981467ece9c8b3e65d893fc377905a6a9cad42f5
Robert Carswell <rcarswel@westga.edu>
2016-02-10 15:10:42 -0500

    Make DataEntryFragment communicate with the activity.

--- app/src/main/java/edu/westga/cs6242/robertcarswellstaticfragments/DataEntryFragment.java
+++ app/src/main/java/edu/westga/cs6242/robertcarswellstaticfragments/DataEntryFragment.java
@@ -1,65 +1,58 @@
 package edu.westga.cs6242.robertcarswellstaticfragments;
 
-
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DataEntryFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class DataEntryFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private EditText number1Text;
+    private EditText number2Text;
+    private DataEntryListener listener;
 
     public DataEntryFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DataEntryFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DataEntryFragment newInstance(String param1, String param2) {
-        DataEntryFragment fragment = new DataEntryFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_data_entry, container, false);
+        View theView = inflater.inflate(R.layout.fragment_data_entry, container, false);
+
+        this.number1Text = (EditText) theView.findViewById(R.id.editView1);
+        this.number2Text = (EditText) theView.findViewById(R.id.editView2);
+
+        Button multiplyButton = (Button) theView.findViewById(R.id.button1);
+        multiplyButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                multiplyButtonClicked();
+            }
+        });
+
+        return theView;
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.listener = (DataEntryListener) context;
+    }
+
+    private void multiplyButtonClicked() {
+        double number1 = Double.parseDouble(this.number1Text.getText().toString());
+        double number2 = Double.parseDouble(this.number2Text.getText().toString());
+
+        this.listener.onDataEntry(number1, number2);
+    }
+
+    public interface DataEntryListener {
+        void onDataEntry(Double number1, Double number2);
+    }
 }
