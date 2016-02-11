75e1854e8642c547d92ecfadb955f55b72484904
rcarswel <rcarswel@westga.edu>
2016-02-10 21:28:07 -0500

    Create the DataAdditionFragment layout.

---
+++ app/src/main/java/edu/westga/cs6242/robertcarswellstaticfragments/DataAdditionFragment.java
@@ -0,0 +1,29 @@
+package edu.westga.cs6242.robertcarswellstaticfragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DataAdditionFragment extends Fragment {
+
+
+    public DataAdditionFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_data_addition, container, false);
+    }
+
+}
