1cd59656244589f30e46cd56831333b8f641350c
rcarswel <rcarswel@westga.edu>
2016-02-10 22:16:33 -0500

    Modify DataAdditionFragment to calculate and display the addition.

--- app/src/main/java/edu/westga/cs6242/robertcarswellstaticfragments/DataAdditionFragment.java
+++ app/src/main/java/edu/westga/cs6242/robertcarswellstaticfragments/DataAdditionFragment.java
@@ -1,29 +1,46 @@
 package edu.westga.cs6242.robertcarswellstaticfragments;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class DataAdditionFragment extends Fragment {
-
+    private TextView textView;
+    private Double number1;
+    private Double number2;
+    private Double addition;
 
     public DataAdditionFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_data_addition, container, false);
+
+        View theView = inflater.inflate(R.layout.fragment_data_display, container, false);
+
+        this.textView = (TextView) theView.findViewById(R.id.textView1);
+
+        return theView;
     }
 
+    public void setNumber1(Double number1) {
+        this.number1 = number1;
+    }
+
+    public void setNumber2(Double number2) {
+        this.number2 = number2;
+    }
+
+    public void addition() {
+        this.addition = this.number1 * this.number2;
+    }
+
+    public void displayAddition() {
+        this.textView.setText(String.format("%.2f", this.addition));
+    }
 }
