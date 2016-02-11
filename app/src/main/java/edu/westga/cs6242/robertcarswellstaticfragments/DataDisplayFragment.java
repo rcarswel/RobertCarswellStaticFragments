319d15d3c52a579d693ae8abe40bdc5c2112b6a8
rcarswel <rcarswel@westga.edu>
2016-02-10 22:11:37 -0500

    Make DataDisplayFragment communicate with the activity

--- app/src/main/java/edu/westga/cs6242/robertcarswellstaticfragments/DataDisplayFragment.java
+++ app/src/main/java/edu/westga/cs6242/robertcarswellstaticfragments/DataDisplayFragment.java
@@ -1,14 +1,17 @@
 package edu.westga.cs6242.robertcarswellstaticfragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DataDisplayFragment extends Fragment {
     private TextView textView;
+    private DataDisplayListener listener;
     private Double number1;
     private Double number2;
     private Double product;
@@ -23,9 +26,28 @@ public class DataDisplayFragment extends Fragment {
 
         View theView = inflater.inflate(R.layout.fragment_data_display, container, false);
 
+        Button additionButton = (Button) theView.findViewById(R.id.button1);
+        additionButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                additionButtonClicked();
+            }
+        });
+
         this.textView = (TextView) theView.findViewById(R.id.textView1);
 
         return theView;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.listener = (DataDisplayListener) context;
+    }
+
+    private void additionButtonClicked() {
+        this.listener.onDataDisplay(this.number1, this.number2);
     }
 
     public void setNumber1(Double number1) {
@@ -42,5 +64,9 @@ public class DataDisplayFragment extends Fragment {
 
     public void displayProduct() {
         this.textView.setText(String.format("%.2f", this.product));
+    }
+
+    public interface DataDisplayListener {
+        void onDataDisplay(Double number1, Double number2);
     }
 }
